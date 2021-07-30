ab92fce40acd7369d94aa207fd2459d89d117060
Joscelyn CUEILLE <jcueille@e2r3p12.42.fr>
2021-07-30 15:29:27 +0200

    save

--- d00/ex02/Account.cpp
+++ d00/ex02/Account.cpp
@@ -1,5 +1,6 @@
 #include "Account.hpp"
 #include <iostream>
+#include <time.h>
 
 /*********************************
 * 		Con/de structors	 	 *
@@ -18,9 +19,39 @@ Account::Account( int initial_deposit )
 }
 Account::~Account( void )
 {
+	Account::_displayTimestamp();
+	std::cout << "  index" << this->_accountIndex << ";amount:" << this->_amount << ";closed" << std::endl;
 	return ;
 }
 
+void Account::_displayTimestamp(void){
+	time_t t;
+	struct tm * timeinfo;
+
+	time(&t);
+	timeinfo = localtime (&t);
+	std::cout << timeinfo->tm_year + 1900 << timeinfo->tm_mday << timeinfo->tm_mon 	<< "_" << timeinfo->tm_hour << timeinfo->tm_min << timeinfo->tm_sec << std::endl;
+}
+
+
+/*********************************
+* 		Public functions	 	 *
+*********************************/
+void	makeDeposit( int deposit ){
+	this->_amount += deposit;
+	this->_nbDeposits++;
+	Account::_totalNbDeposits++;
+	Account::_totalAmount += deposit;
+}
+	bool	makeWithdrawal( int withdrawal ){
+		if (withdrawal >  this->amount)
+			return (false);
+		this->amount -= withdrawal;
+		this->_nbWithdrawals++;
+		Account::_totalNbWithdrawals++;
+		Account::_totalAmount -= withdrawal;
+	}
+	
 
 /*********************************
 * 		Static functions	 	 *
@@ -54,3 +85,6 @@ void	Account::displayAccountsInfos( void )
 }
 
 int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
