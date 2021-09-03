ee76c5e814c0850633e9b16cf2b2142b0e5fb0e8
Joscelyn CUEILLE <jcueille@e2r7p9.42.fr>
2021-09-03 15:34:54 +0200

    fragtrap

---
+++ d03/ex02/FragTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef FRAGTRAP_HPP
+# define FRAGTRAP_HPP
+
+# include <iostream>
+# include <string>
+# 
+
+class FragTrap : public ClapTrap
+{
+
+	public:
+
+		FragTrap();
+		FragTrap( FragTrap const & src );
+		~FragTrap();
+
+		FragTrap &		operator=( FragTrap const & rhs );
+
+	private:
+
+};
+
+std::ostream &			operator<<( std::ostream & o, FragTrap const & i );
+
+#endif /* ******************************************************** FRAGTRAP_H */
--- d03/ex02/ScavTrap.hpp
+++ d03/ex02/ScavTrap.hpp
@@ -1,24 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jcueille <jcueille@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/08/31 18:45:36 by jcueille          #+#    #+#             */
-/*   Updated: 2021/09/03 15:21:06 by jcueille         ###   ########.fr       */
+/*   Created: 2021/09/02 15:39:06 by jcueille          #+#    #+#             */
+/*   Updated: 2021/09/02 16:29:03 by jcueille         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScavTrap.hpp"
+#ifndef ScavTRAP_HPP
+# define ScavTRAP_HPP
+#include "ClapTrap.hpp"
 
-int main( void )
-{
-	ScavTrap clap("Jo");
+class ScavTrap : public ClapTrap {
+	public:
+	ScavTrap( void );
+	ScavTrap(std::string name);
+	ScavTrap(ScavTrap const & clp);
+	~ScavTrap( void );
+	void attack(std::string const & target);
+	void guardGate();
 	
-	clap.attack("trap");
-	clap.takeDamage(5);
-	clap.beRepaired(3);
-	clap.guardGate();
-	return 0;
-}
+};
+
+#endif
